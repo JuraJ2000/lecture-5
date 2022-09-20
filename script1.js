391d20a2941d2480f2d1e129dce169b510576ed7
juraj95 <juraj8546@gmail.com>
2022-09-20 13:48:46 +0300

    all done

---
+++ script1.js
@@ -0,0 +1,7 @@
+const mybutton = document.getElementById("button");
+mybutton.addEventListener("mouseover", function () {
+  mybutton.style.backgroundColor = "blue";
+});
+mybutton.addEventListener("mouseout", function () {
+  mybutton.style.backgroundColor = "white";
+});
